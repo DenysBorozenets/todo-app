c64fcfe1ccac69d9ba1fc741be8fa3d00b10d204
DenysBorozenets <borozenetsdenysforme@gmail.com>
2022-08-10 18:13:48 +0300

    initial commit

---
+++ src/main/java/com/denis/todoapp/controllers/ToDoMainController.java
@@ -0,0 +1,15 @@
+package com.denis.todoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ToDoMainController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+}
